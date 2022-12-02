7932289392857facbc6127e212f9512995acc10c
Daniel <danya.ostap@gmail.com>
2022-12-02 23:13:41 +0200

    Added dynamic queues load

---
+++ src/axios/requests.js
@@ -0,0 +1,5 @@
+export const ENDPOINTS = {
+    GET_QUEUES: "",
+    CREATE_QUEUE: "",
+    ENROLL_TO_QUEUE: "",
+}
--- src/components/Queues.js
+++ src/components/Queues.js
@@ -1,7 +1,24 @@
-import React, { Fragment } from "react";
-import { Table, Form, Button } from "react-bootstrap";
+import React, { Fragment, useEffect, useState } from "react";
+import { Table, Form, Button, Spinner } from "react-bootstrap";
+import axios from "axios";
+import { ENDPOINTS } from "../axios/requests";
 
 const Queues = () => {
+  const [queues, setQueues] = useState(null);
+
+  useEffect(() => {
+    axios({
+      url: ENDPOINTS.GET_QUEUES,
+      method: "get",
+    })
+      .then((response) => {
+        setQueues(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Fragment>
       <div className="queue-header">
@@ -17,20 +34,50 @@ const Queues = () => {
           <Button variant="primary">Create</Button>
         </div>
       </div>
-      <Table striped hover>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date Created</th>
-            <th>Event Date</th>
-            <th>People in queue</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          
-        </tbody>
-      </Table>
+
+      {queues !== null ? (
+        <Table striped hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date Created</th>
+              <th>Event Date</th>
+              <th>People in queue</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {queues.map((queue) => {
+              return (
+                <tr>
+                  <td>{queue.name}</td>
+                  <td>{queue.dateCreated}</td>
+                  <td>{queue.eventDate}</td>
+                  <td>{queue.peopleAmount}</td>
+                  <td>
+                    <Button variant="primary">Enroll</Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "center",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+            paddingTop: "150px",
+          }}
+        >
+          <Spinner style={{ margin: "15px" }} />
+          <h4>Loading queues</h4>
+        </div>
+      )}
     </Fragment>
   );
 };

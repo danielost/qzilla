037d8cead235fcb78eac6f65ad8aff81db3b3ed8
Daniel <danya.ostap@gmail.com>
2022-12-06 14:16:44 +0200

    Added the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React from "react";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCheckbox,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import Logo from "../imgs/logo.png";
 
 const Home = () => {
-    return (<></>)
-}
+  return (
+    <MDBContainer fluid className="p-4">
+      <MDBRow>
+        <MDBCol
+          md="6"
+          className="text-center text-md-start d-flex flex-column justify-content-center"
+        >
+          <h1 className="my-5 display-3 fw-bold ls-tight px-3">
+            The best offer <br />
+            <span className="text-primary">for your events</span>
+          </h1>
+
+          <p className="px-3" style={{ color: "hsl(217, 10%, 50.8%)" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+            itaque accusantium odio, soluta, corrupti aliquam quibusdam tempora
+            at cupiditate quis eum maiores libero veritatis? Dicta facilis sint
+            aliquid ipsum atque?
+          </p>
+        </MDBCol>
+
+        <MDBCol
+          md="6"
+          style={{
+            display: "table-cell",
+            verticalAlign: "middle",
+            textAlign: "center",
+          }}
+        >
+          <img src={Logo} width="400px" />
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
 
 export default Home;
